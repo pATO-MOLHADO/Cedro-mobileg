e28c08790304c7aacd51138023ace1a491d33cb7
pATO-MOLHADO <rm95262@estudante.fieb.edu.br>
2026-03-30 16:08:43 -0300

    fix snack: default export + metro config

--- metro.config.js
+++ metro.config.js
@@ -1,19 +1,2 @@
 const { getDefaultConfig } = require('expo/metro-config');
-const config = getDefaultConfig(__dirname);
-
-config.watchFolders = [__dirname];
-config.resolver.useWatchman = false;
-config.watcher = {
-  watchman: { deferStates: [] },
-  health: { enabled: false },
-  additionalExts: [],
-  useWatchman: false,
-  fs: {
-    polling: {
-      enabled: true,
-      interval: 2000,
-    },
-  },
-};
-
-module.exports = config;
+module.exports = getDefaultConfig(__dirname);
